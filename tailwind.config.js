28f6a2947574a94734b1d767bed82c098ea175bc
grJDh <rusmukhametov@gmail.com>
2022-06-24 17:10:33 +0300

    Added tailwind's dependencies and updated template paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {
       fontFamily: {
