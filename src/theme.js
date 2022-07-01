d27d83328d8b427fc9cf24a2eab5ffaf7c7d5647
grJDh <rusmukhametov@gmail.com>
2022-07-01 20:31:34 +0300

    Added more colors

--- src/theme.js
+++ src/theme.js
@@ -3,8 +3,20 @@ const theme = {
     red950: '#0D0404',
     red50: '#FFF2F2',
 
+    green950: '#040D06',
+    green50: '#F2FFF5',
+
+    blue950: '#04090D',
+    blue50: '#F2F9FF',
+
     redAlpha100: '#FF4C4C26',
     redAlpha700: '#FF4C4Cb5',
+
+    greenAlpha100: '#4DDA6E26',
+    greenAlpha700: '#4DDA6Eb5',
+    
+    blueAlpha100: '#4CACFF26',
+    blueAlpha700: '#4CACFFb5',
   },
   font: {
     family: {
